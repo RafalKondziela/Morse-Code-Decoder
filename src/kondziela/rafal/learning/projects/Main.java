cd958c655e061d1400e70dbb646af691d7dc017a
Rafał Kondziela <rafal.kondziela@gmail.com>
2019-08-03 10:43:29 +0200

    Simple cl interface and config file to morse code (not inclding digits or special signs yet)

--- src/kondziela/rafal/learning/projects/Main.java
+++ src/kondziela/rafal/learning/projects/Main.java
@@ -1,5 +1,7 @@
 package kondziela.rafal.learning.projects;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,7 +10,32 @@ public class Main {
 
     while (isOver == false){
 
-        System.out.println("Welcome in Morse Code decoding program type 'help' to get list of avaliable commands");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome in Morse Code decoding program type 'help' to get list of available commands");
+
+
+         String key = scanner.next();
+
+
+
+        switch (key){
+
+
+
+            case "exit":
+                isOver = true;
+                break;
+
+            case "help":
+                System.out.println("Available commands : ");
+                System.out.println("exit - exit application");
+                System.out.println("config - show the Morse Code key");
+
+            default:
+                System.out.println("Wrong command type 'help' or wright command to continiue");
+                break;
+        }
+
     }
 
 
